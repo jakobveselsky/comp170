c9b3da32204de752c0c2c25495da2bcc2d6facc8
jakobveselsky <54648644+jakobveselsky@users.noreply.github.com>
2019-10-23 13:26:36 -0500

    Add files via upload

---
+++ Iterate/Iterate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterate
+{
+    class Program
+    {
+        private static object yeild;
+
+        static void Main(string[] args)
+        {
+            List<int> things = new List<int>();
+            int a = 0;
+            int b = 0;
+
+            Console.WriteLine("Enter number one: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number two: ");
+            b = int.Parse(Console.ReadLine());
+
+            for(int k = a; k < b+1; k++)
+            {
+                things.Add(k);
+            }
+            foreach (int number in RemoveOdd(a, b))
+            {
+                things.Remove(number);
+            }
+            Console.WriteLine("");
+            foreach (int l in things)
+            {
+                Console.WriteLine(l);
+            }
+        }
+
+        static IEnumerable<int> RemoveOdd(int a, int b)
+        {
+            for (int i = a; i < b + 1; i = i + 2)
+            {
+                yield return i;
+            }
+        }
+    }
+}
